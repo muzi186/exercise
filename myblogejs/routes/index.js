fbf743ea45d69de1d1154749e8d017ca19fa5954
gavin <gavin2lee@163.com>
2016-04-15 19:01:03 +0800

    migrate to 64bit

--- myblogejs/routes/index.js
+++ myblogejs/routes/index.js
@@ -6,4 +6,35 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'MyBlog',username: 'Gavin Li' });
 });
 
+//Added by Gavin
+router.get('/u/:user',function(req,res,next){
+	res.send('U');
+});
+
+router.post('/post',function(req.res.next){
+	res.send('post');
+});
+
+router.get('/reg', function(req, res,next){
+	res.send('reg');
+});
+
+router.post('/reg', function(req, res, next){
+	res.send('reg post');
+});
+
+
+router.get('/login',function(req,res,next){
+    res.send("Login Page");
+});
+
+router.post('/login', function(req, res, next){
+	res.send('login');
+});
+
+router.get('/logout', function(req, res, next){
+	res.send('logout');
+});
+
+
 module.exports = router;
