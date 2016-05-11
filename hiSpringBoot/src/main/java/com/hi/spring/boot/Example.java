845560ecaeb717d600369fc8e993512acfa2a929
gavin <gavin2lee@163.com>
2016-05-11 16:55:32 +0800

    add gradle

--- hiSpringBoot/src/main/java/com/hi/spring/boot/Example.java
+++ hiSpringBoot/src/main/java/com/hi/spring/boot/Example.java
@@ -3,6 +3,7 @@ package com.hi.spring.boot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,8 +11,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class Example {
 	
 	@RequestMapping("/")
-	String home(){
-		return "Hello SpringBoot!";
+	String home(@RequestParam(value="name", defaultValue="Spring Boot") String name){
+		String template = "Hello %s!";
+		String result = String.format(template, name);
+		return result;
 	}
 
 	public static void main(String[] args) {
